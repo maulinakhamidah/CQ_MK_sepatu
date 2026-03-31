e5e540e45bcfba9cecffa24cc5ff8d0aa7ef0e86
maulinakhamidah <maulinakhamidah115@gmail.com>
2026-03-31 16:50:33 +0700

    feat: menambahkan variabel koneksi database

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Data.SqlClient;(\\done)
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -13,7 +13,7 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form 
     {
-        SqlConnection conn;
+        SqlConnection conn;()
         SqlCommand cmd;
 
         public Form1()
